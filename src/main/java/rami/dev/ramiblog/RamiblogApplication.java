7ab8bd920a6714e09a5f886e19aa84b63c6cb6b6
wooram <rla7360@gmail.com>
2023-04-19 21:17:32 +0900

    initial commit

---
+++ src/main/java/rami/dev/ramiblog/RamiblogApplication.java
@@ -0,0 +1,13 @@
+package rami.dev.ramiblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RamiblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RamiblogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rami/dev/ramiblog/RamiblogApplicationTests.java
@@ -0,0 +1,13 @@
+package rami.dev.ramiblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RamiblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
