761d198a55d2730480011180464c8f5bc390c841
wooram <rla7360@gmail.com>
2023-04-28 19:58:28 +0900

    파일 업로드 완료

---
+++ src/main/java/rami/dev/ramiblog/core/util/MyFileUtil.java
@@ -0,0 +1,25 @@
+package rami.dev.ramiblog.core.util;
+
+import org.springframework.web.multipart.MultipartFile;
+import rami.dev.ramiblog.core.exception.ssr.Exception500;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+public class MyFileUtil {
+    public static String write(String uploadFolder, MultipartFile file) {
+        //롤링 기법
+        UUID uuid = UUID.randomUUID();
+        String originalFilename = file.getOriginalFilename();
+        String uuidFilename = uuid + "_" + originalFilename;
+        try {
+            Path filePath = Paths.get(uploadFolder + uuidFilename);
+            Files.write(filePath, file.getBytes());
+        } catch (Exception e) {
+            throw new Exception500("파일 업로드 실패 : "+e.getMessage());
+        }
+        return uuidFilename;
+    }
+}
--- src/main/java/rami/dev/ramiblog/service/UserService.java
+++ src/main/java/rami/dev/ramiblog/service/UserService.java
@@ -1,10 +1,16 @@
 package rami.dev.ramiblog.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+import rami.dev.ramiblog.core.exception.ssr.Exception400;
+import rami.dev.ramiblog.core.exception.ssr.Exception500;
+import rami.dev.ramiblog.core.util.MyFileUtil;
 import rami.dev.ramiblog.dto.user.UserRequest;
+import rami.dev.ramiblog.model.user.User;
 import rami.dev.ramiblog.model.user.UserRepository;
 
 @Service
@@ -12,6 +18,9 @@ import rami.dev.ramiblog.model.user.UserRepository;
 public class UserService {
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder passwordEncoder;
+
+    @Value("${file.path}")
+    private String uploadFolder;
 
 
     // insert, update, delete -> try catch 처리
@@ -27,4 +36,26 @@ public class UserService {
         }
 
     } // 더티체킹, DB 세션 종료 (OSIV=false)
+
+
+    public User 회원프로필보기(Long id) {
+        User userPS = userRepository.findById(id)
+                .orElseThrow(()->new Exception400("id", "해당 유저가 존재하지 않습니다"));
+        return userPS;
+    }
+
+    @Transactional
+    public User 프로필사진수정(MultipartFile profile, Long id) {
+        try {
+            String uuidImageName = MyFileUtil.write(uploadFolder, profile);
+
+
+            User userPS = userRepository.findById(id)
+                    .orElseThrow(()->new Exception500("로그인 된 유저가 DB에 존재하지 않음"));
+            userPS.changeProfile(uuidImageName);
+            return userPS;
+        }catch (Exception e){
+            throw new Exception500("프로필 사진 등록 실패 : "+e.getMessage());
+        }
+    }
 }
